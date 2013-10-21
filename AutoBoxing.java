90f55547023bc725063795ad0cb1dab32ca153a4
Jonathan Lai <jonathanlai@eltux.net>
2013-10-21 08:44:46 -0700

    chap/day 3 small exercises

---
+++ AutoBoxing.java
@@ -0,0 +1,10 @@
+
+class AutoBoxing {
+    public static void main(String[] arguments) {
+		    
+		Float f1 = new Float(12.5F);
+		Float f2 = new Float(27.2F);
+		System.out.println("Lower number: " + Math.min(f1, f2));
+
+    }
+}
---
+++ StringConcatenation.java
@@ -0,0 +1,9 @@
+
+
+class StringConcatenation {
+    
+    public static void main(String arguments[]){
+       float gpa = 2.25F;
+       System.out.println("Honest, dad, my GPA is a " + (gpa+1.5));  
+    }
+}
