7eeb7784d35404f1d3fb4159e29df45b1a9667c5
Jonathan Lai <jonathanlai@eltux.net>
2013-10-22 14:10:03 -0700

    more files, day 3 thoughts

---
+++ JavaVsGroovy.groovy
@@ -0,0 +1,14 @@
+
+/*class JavaVsGroovy {
+
+    public static void main(){
+        println "hello world."
+    }
+}
+*/
+
+
+//Wow, the above script is not needed/does not work. I still wonder if/how I can emulate classes in Groovy scripts
+//to run with groovyc Something.groovy; groovy Something
+
+println "Hello World."
