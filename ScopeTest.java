a4445eb64a37fa8967ba5607f3888f3561b9eb90
Jonathan Lai <jonathanlai@eltux.net>
2013-10-30 19:48:29 -0700

    add ScopeTest.java, Classes and Methods Chapter

---
+++ ScopeTest.java
@@ -0,0 +1,13 @@
+class ScopeTest {
+  int test = 10;
+
+  void printTest() {
+    int test = 20;
+    System.out.println("Test: " + test);
+  }
+
+  public static void main(String[] arguments) {
+    ScopeTest st = new ScopeTest();
+    st.printTest();
+  }
+}
